b744fff916dd597c56e5a644c5d90c48b07723d1
vhehfhvl <vhehfnvl@committer.co.kr>
2020-06-24 02:47:31 +0900

    materialUIIcon&SearchFunc

--- team3/src/App.js
+++ team3/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Link } from 'react-router-dom';
 import { IconButton, TextField, CardHeader, CardMedia, CardContent, CardActions, Avatar, Container, Paper, Divider, Textfield, Input, FormControl, Button, ButtonGroup, InputLabel, Typography, Grid, Card, OutlinedInput } from '@material-ui/core/';
 import { makeStyles, withStyles } from '@material-ui/styles';
+import SearchIcon from '@material-ui/icons/Search';
 
 function App() {
+
+
+
+  const [postList, setPostList] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [changedPostList, setChangedPostList] = useState('');
+
+
+  const onChangeSearchValue = (e) => setSearchValue(e.target.value);
+
+  const onSearch = (e) => {
+    e.preventDefault();
+    // const searchResult = postList.filter(post => post.title.indexOf(searchValue) !== -1);
+    // setChangedPostList(searchResult);
+    // setSearchValue('');
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <form onSubmit={onSearch}>
+          <TextField variant="outlined" value={searchValue} onChange={onChangeSearchValue}/>
+          <IconButton type="submit" aria-label="search" onSubmit={onSearch}>
+            <SearchIcon />
+          </IconButton>
+      </form>
+
       </header>
     </div>
   );
