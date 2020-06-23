225bc1400fd674e36c345fae20dd4575b5f6c0d9
vhehfhvl <vhehfnvl@committer.co.kr>
2020-06-24 02:58:59 +0900

    SearchResult with popup

--- team3/src/App.js
+++ team3/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Link } from 'react-router-dom';
-import { IconButton, TextField, CardHeader, CardMedia, CardContent, CardActions, Avatar, Container, Paper, Divider, Textfield, Input, FormControl, Button, ButtonGroup, InputLabel, Typography, Grid, Card, OutlinedInput } from '@material-ui/core/';
+import { IconButton, TextField, Dialog, DialogContent, DialogActions, Button} from '@material-ui/core/';
 import { makeStyles, withStyles } from '@material-ui/styles';
 import SearchIcon from '@material-ui/icons/Search';
 
@@ -13,12 +13,14 @@ function App() {
   const [postList, setPostList] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const [changedPostList, setChangedPostList] = useState('');
+  const [searchResult, setSearchResult] = useState(false);
 
 
   const onChangeSearchValue = (e) => setSearchValue(e.target.value);
 
   const onSearch = (e) => {
     e.preventDefault();
+    setSearchResult(true)
     // const searchResult = postList.filter(post => post.title.indexOf(searchValue) !== -1);
     // setChangedPostList(searchResult);
     // setSearchValue('');
@@ -30,12 +32,28 @@ function App() {
     <div className="App">
       <header className="App-header">
 
+      <div className="App-title">Team3 - Subject Information</div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+
       <form onSubmit={onSearch}>
           <TextField variant="outlined" value={searchValue} onChange={onChangeSearchValue}/>
           <IconButton type="submit" aria-label="search" onSubmit={onSearch}>
             <SearchIcon />
           </IconButton>
       </form>
+
+      <Dialog open={searchResult} onClose={!searchResult}>
+          <DialogContent>
+              {/* <p className={styles['position-name']}>{selectedPositionName}</p>
+              <div className={styles['job-description']}>{description}</div> */}
+          </DialogContent>
+          <DialogActions>
+              <Button variant="outlined" color="primary" onClick={() => {setSearchResult(!searchResult)}}>Close</Button>
+          </DialogActions>
+      </Dialog>
 
       </header>
     </div>
